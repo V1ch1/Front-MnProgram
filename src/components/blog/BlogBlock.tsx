cd03b96bfef6dddfc9718add3fc6e02bee3a071f
Jose Blanco <blancocasal@hotmail.com>
2024-12-10 15:56:46 +0100

    Fix BlockBlock

--- src/components/blog/BlogBlock.tsx
+++ src/components/blog/BlogBlock.tsx
@@ -14,58 +14,61 @@ const BlogBlock: React.FC = () => {
 
     const { blog } = currentPage;
 
-  return (
-    <div className="blog-block container">
-      <div className="header">
-        <TypingEffect
-          fixedText="25 años"
-          typingTexts={["dando soporte", "creciendo contigo", "a tu lado"]}
-          fixedTextColor="#000000" // Negro para el texto fijo
-          typingTextColor="#FD4B5C" // Naranja para el texto dinámico
-        />
-      </div>
+    return (
+      <div className="blog-block container">
+        <div className="header">
+          <TypingEffect
+            fixedText="25 años"
+            typingTexts={["dando soporte", "creciendo contigo", "a tu lado"]}
+            fixedTextColor="#000000" // Negro para el texto fijo
+            typingTextColor="#FD4B5C" // Naranja para el texto dinámico
+          />
+        </div>
 
-      <div className="left-column">
-        <img
-          src={blog.leftColumn.image}
-          alt="Imagen de la izquierda"
-          className="blog-image"
-        />
-        <h2 className="blog-title">{blog.leftColumn.title}</h2>
-        <p
-          className="blog-text"
-          dangerouslySetInnerHTML={{ __html: blog.leftColumn.text }}
-        />
-        <a
-          href={blog.leftColumn.button.url}
-          target="_blank"
-          className="blog-button"
-        >
-          {blog.leftColumn.button.text}
-        </a>
-      </div>
+        <div className="left-column">
+          <img
+            src={blog.leftColumn.image}
+            alt="Imagen de la izquierda"
+            className="blog-image"
+          />
+          <h2 className="blog-title">{blog.leftColumn.title}</h2>
+          <p
+            className="blog-text"
+            dangerouslySetInnerHTML={{ __html: blog.leftColumn.text }}
+          />
+          <a
+            href={blog.leftColumn.button.url}
+            target="_blank"
+            className="blog-button"
+          >
+            {blog.leftColumn.button.text}
+          </a>
+        </div>
 
-      <div className="right-column">
-        <img
-          src={blog.rightColumn.image}
-          alt="Imagen de la derecha"
-          className="blog-image"
-        />
-        <h2 className="blog-title">{blog.rightColumn.title}</h2>
-        <p
-          className="blog-text"
-          dangerouslySetInnerHTML={{ __html: blog.rightColumn.text }}
-        />
-        <a
-          href={blog.rightColumn.button.url}
-          target="_blank"
-          className="blog-button"
-        >
-          {blog.rightColumn.button.text}
-        </a>
+        <div className="right-column">
+          <img
+            src={blog.rightColumn.image}
+            alt="Imagen de la derecha"
+            className="blog-image"
+          />
+          <h2 className="blog-title">{blog.rightColumn.title}</h2>
+          <p
+            className="blog-text"
+            dangerouslySetInnerHTML={{ __html: blog.rightColumn.text }}
+          />
+          <a
+            href={blog.rightColumn.button.url}
+            target="_blank"
+            className="blog-button"
+          >
+            {blog.rightColumn.button.text}
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <div>Página no encontrada</div>;
+  }
 };
 
 export default BlogBlock;
