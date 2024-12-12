d5887d743efaf2af7d6b34e8cf93f26a007b2bcd
Jose Blanco <blancocasal@hotmail.com>
2024-12-12 23:12:31 +0100

    add buttons fixed bottom

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Odontologos from "./pages/Odontologos";
 import Arquitectos from "./pages/Arquitectos";
 import Procuradores from "./pages/Procuradores";
 import Servicios from "./pages/Servicios";
+import FixedButtons from "./components/FixedButtons/FixedButtons";
 
 const App: React.FC = () => {
   return (
@@ -34,6 +35,7 @@ const App: React.FC = () => {
           <Route path="/software-servicios-mk" element={<Servicios />} />
           <Route path="/test-page" element={<TestPage />} />
         </Routes>
+        <FixedButtons />
       </Router>
     </AppProvider>
   );
---
+++ src/components/FixedButtons/FixedButtons.tsx
@@ -0,0 +1,25 @@
+// components/FixedButtons.tsx
+import React from "react";
+import VerPreciosButton from "../buttons/VerPreciosButton";
+import VerVideoButton from "../buttons/VerVideoButton";
+
+const FixedButtons: React.FC = () => {
+  return (
+    <div className="fixed bottom-28 left-5 z-50 flex flex-col gap-2">
+      {/* Botón de Ver Precios */}
+      <VerPreciosButton
+        label="Ver Precios"
+        location="bottom-left"
+        logoUrl="/path/to/logo.png"
+      />
+      {/* Botón de Ver Vídeo */}
+      <VerVideoButton
+        label="Ver Vídeo"
+        location="bottom-left"
+        videoUrl="/path/to/video.mp4"
+      />
+    </div>
+  );
+};
+
+export default FixedButtons;
